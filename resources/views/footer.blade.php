31e191172b4704085d35205bf571a6d03b68e6c3
Facu <lecuisine22@gmail.com>
2019-11-16 12:37:02 -0300

    faq

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,97 @@
+
+@section("footer")
+<footer class="page-footer font-small indigo">
+<!-- Footer Links -->
+  <div class="container">
+    <!-- Grid row-->
+    <hr class="rgba-white-light" style="margin: 0 5%;">
+
+    <div class="row text-center d-flex justify-content-center pt-5 mb-3 footer2">
+      <!-- Grid column -->
+      <div class="col-md-2 mb-3">
+        <h6 class="text-uppercase font-weight-bold">
+          <a href="#!">Sobre Nosotros</a>
+        </h6>
+      </div>
+      <!-- Grid column -->
+
+      <!-- Grid column -->
+      <div class="col-md-2 mb-3">
+        <h6 class="text-uppercase font-weight-bold">
+          <a href="sumary-art.php">Productos</a>
+        </h6>
+      </div>
+      <!-- Grid column -->
+
+      <div class="col-md-2 mb-3">
+        <h6 class="text-uppercase font-weight-bold">
+          <a href="faq.php">FAQ</a>
+        </h6>
+      </div>
+      <!-- Grid column -->
+
+      <!-- Grid column -->
+      <div class="col-md-2 mb-3">
+        <h6 class="text-uppercase font-weight-bold">
+          <a href="contacto.php">Contacto</a>
+        </h6>
+      </div>
+      <!-- Grid column -->
+
+    </div>
+    <!-- Grid row-->
+    <hr class="rgba-white-light" style="margin: 0 15%;">
+
+    <!-- Grid row-->
+    <div class="row d-flex text-center justify-content-center mb-md-0 mb-4">
+
+      <!-- Grid column -->
+      <div class="col-md-8 col-12 mt-3">
+        <p style="line-height: 1.7rem">Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+          accusantium doloremque laudantium, totam rem
+          aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt
+          explicabo.
+          Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur.</p>
+      </div>
+      <!-- Grid column -->
+      <!-- Links redes sociales -->
+    <div id="social" class="col-md-5">
+      <ul class="list-unstyled list-inline text-center">
+        <li class="list-inline-item">
+          <a class="btn-floating btn-fb mx-1">
+            <i class="fab fa-facebook-f"> </i>
+          </a>
+        </li>
+        <li class="list-inline-item">
+          <a class="btn-floating btn-tw mx-1">
+            <i class="fab fa-twitter"> </i>
+          </a>
+        </li>
+        <li class="list-inline-item">
+          <a class="btn-floating btn-gplus mx-1">
+            <i class="fab fa-google-plus-g"> </i>
+          </a>
+        </li>
+        <li class="list-inline-item">
+          <a class="btn-floating btn-li mx-1">
+            <i class="fab fa-linkedin-in"> </i>
+          </a>
+        </li>
+        <li class="list-inline-item">
+          <a class="btn-floating btn-dribbble mx-1">
+            <i class="fab fa-dribbble"> </i>
+          </a>
+        </li>
+      </ul>
+    </div>
+      <!-- Links redes sociales -->
+    </div>
+    <!-- Grid row-->
+    <hr class="clearfix d-md-none rgba-white-light" style="margin: 10% 15% 5%;">
+  </div>
+
+  <div class="footer-copyright text-center py-3 footer2">© 2019 Copyright:
+    <a href="#"> Grupo 2 | DH</a>
+  </div>
+  </footer>
+  @stop
---
+++ resources/views/header.blade.php
@@ -0,0 +1,44 @@
+
+@section("header")
+<header>
+  
+<nav class="navbar navbar-expand-md navbar-dark bg-info mb-3 sombra">
+    <div class="container-fluid">
+      <div class="header-row">
+        <div class="logo-search">
+          <div class="logo-cont col-sm-9 col-md-3">
+            <a href="#" class=""><img src="img/1.jpg" alt="" class="logo"></a>
+          </div>
+
+          
+        </div>
+
+        <div class="navbar-option">
+          <div class="navbar-desp col-sm-12">
+
+            <button type="button" class="navbar-toggler col-sm-12" data-toggle="collapse" data-target="#navbarCollapse">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarCollapse">
+
+              <ul class="navbar-nav">
+                <li class="nav-item active"><a href="index.php" class="nav-item nav-link active" id="item">Home</a></li>
+                <li class="nav-item"><a href="sumary-art.php" class="nav-item nav-link active" id="item">Vinilos</a></li>
+                <li class="nav-item"><a href="faq.php" class="nav-item nav-link active" id="item">FAQ</a></li>
+                <li class="nav-item"><a href="contacto.php" class="nav-item nav-link active" id="item">Contacto</a></li>
+              </ul>
+
+              <ul class="navbar-nav nav-reg">
+                <li class="nav-item"><a class="nav-item nav-link" href="register.php">Register</a></li>
+                <li class="nav-item"><a class="nav-item nav-link" href="login.php">Login</a></li>
+                <li class="nav-item"><a class="nav-item nav-link" a href="carrito.php" id="Carrito">Carrito (<?php echo (empty($_SESSION["CARRITO"]))?0:count($_SESSION["CARRITO"]);?>)</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </nav>
+</header>
+@endsection()
